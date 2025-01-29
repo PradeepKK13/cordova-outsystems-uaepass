6d0133b702d6737b5f95efe27c35a77d3f5cfd5d
PradeepKK13 <51114651+PradeepKK13@users.noreply.github.com>
2025-01-30 03:36:23 +0400

    Update Appdelegate+UAEPass.swift

--- src/ios/Appdelegate+UAEPass.swift
+++ src/ios/Appdelegate+UAEPass.swift
@@ -26,26 +26,26 @@ extension AppDelegate{
         }
     }
     
-    // open override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    //     if #available(iOS 13.0, *) {
-    //         print("<><><><> appDelegate URL : \(url.absoluteString)")
-    //         if url.absoluteString.contains(HandleURLScheme.externalURLSchemeSuccess()) {
-    //             if let topViewController = UserInterfaceInfo.topViewController() {
-    //                 if let webViewController = topViewController as? UAEPassWebViewController {
-    //                     webViewController.forceReload()
-    //                 }
-    //             }
-    //             return true
-    //         } else if url.absoluteString.contains(HandleURLScheme.externalURLSchemeFail()) {
-    //             guard let webViewController = UserInterfaceInfo.topViewController() as? UAEPassWebViewController  else { return false}
-    //             webViewController.foreceStop()
-    //             webViewController.dismiss(animated: true)
-    //             return false
-    //         }
-    //         return false
-    //     } else {
-    //         return false;
-    //     }
-    // }
+    open override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        // if #available(iOS 13.0, *) {
+        //     print("<><><><> appDelegate URL : \(url.absoluteString)")
+        //     if url.absoluteString.contains(HandleURLScheme.externalURLSchemeSuccess()) {
+        //         if let topViewController = UserInterfaceInfo.topViewController() {
+        //             if let webViewController = topViewController as? UAEPassWebViewController {
+        //                 webViewController.forceReload()
+        //             }
+        //         }
+        //         return true
+        //     } else if url.absoluteString.contains(HandleURLScheme.externalURLSchemeFail()) {
+        //         guard let webViewController = UserInterfaceInfo.topViewController() as? UAEPassWebViewController  else { return false}
+        //         webViewController.foreceStop()
+        //         webViewController.dismiss(animated: true)
+        //         return false
+        //     }
+        //     return false
+        // } else {
+            return false;
+        // }
+    }
     
 }
