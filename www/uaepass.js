a7c45c80694d7f6d7d585647f71064bd005dfc96
PradeepKK13 <51114651+PradeepKK13@users.noreply.github.com>
2025-01-30 11:06:27 +0400

    Update uaepass.js

--- www/uaepass.js
+++ www/uaepass.js
@@ -1,27 +1,27 @@
 var exec = require('cordova/exec');
 
-document.addEventListener('deviceready', function() { // Wait for deviceready
-    var channel = cordova.require("cordova/channel");
-    if (channel) {
-        channel.OSDeepLinksHandlerChannel.subscribe(function (url) {
-            console.log("Received deep link URL (event):", url);
+// document.addEventListener('deviceready', function() { // Wait for deviceready
+//     var channel = cordova.require("cordova/channel");
+//     if (channel) {
+//         channel.OSDeepLinksHandlerChannel.subscribe(function (url) {
+//             console.log("Received deep link URL (event):", url);
 
-            // Your custom logic to handle the URL goes here.
-            // Example:
-            if (url.includes("your-success-path")) {
-                // Do something (e.g., navigate to a specific page)
-                window.location.href = "#/success"; // Example using a hash-based route
-            } else if (url.includes("your-failure-path")) {
-                // Do something else
-                window.location.href = "#/failure";
-            } else {
-                // Handle other deep links
-            }
-        });
-    } else {
-      console.error("Cordova channel not available for Deep Link handling");
-    }
-}, false);
+//             // Your custom logic to handle the URL goes here.
+//             // Example:
+//             if (url.includes("your-success-path")) {
+//                 // Do something (e.g., navigate to a specific page)
+//                 window.location.href = "#/success"; // Example using a hash-based route
+//             } else if (url.includes("your-failure-path")) {
+//                 // Do something else
+//                 window.location.href = "#/failure";
+//             } else {
+//                 // Handle other deep links
+//             }
+//         });
+//     } else {
+//       console.error("Cordova channel not available for Deep Link handling");
+//     }
+// }, false);
 
 exports.init = function (success, error,environment,clientID,clientSecret,redirectUrl) {
     this.bindEvents();
